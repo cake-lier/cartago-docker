2271c47fddd84a198a8427adc5da570aadf9a027
Matteo Castellucci <matteo.castellucci@outlook.com>
2023-11-07 12:51:13 +0100

    fix: support argument passing to JAR, env variables from Dockerfile

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,10 @@
 FROM eclipse-temurin:21.0.1_12-jre
 
+ENV ENV_NAME=main
+ENV ROOT_NAME=mas
+ENV PORT=20100
+
 RUN mkdir /opt/app
 COPY build/libs/main.jar /opt/app/
-CMD ["java", "-jar", "/opt/app/main.jar"]
+EXPOSE $PORT
+CMD java -jar /opt/app/main.jar --env-name=$ENV_NAME --root-name=$ROOT_NAME --port=$PORT
--- src/main/java/io/github/cakelier/AppMain.java
+++ src/main/java/io/github/cakelier/AppMain.java
@@ -1,33 +1,24 @@
-package io.cakelier.github;
+package io.github.cakelier;
 
 import cartago.CartagoEnvironment;
 import cartago.CartagoException;
 import cartago.utils.BasicLogger;
-import com.typesafe.config.Config;
-import com.typesafe.config.ConfigFactory;
+import org.apache.commons.cli.*;
 
 public final class AppMain {
-    private static final String ENVIRONMENT_DEFAULT_NAME = "mas";
-    private static final int PORT_DEFAULT_VALUE = 20100;
-
     private AppMain() { }
 
-    public static void main(final String[] args) throws CartagoException {
-        final var config = ConfigFactory.load();
+    public static void main(final String[] args) throws CartagoException, ParseException {
+        final var parser = new DefaultParser().parse(
+            new Options()
+                .addOption(new Option("e", "env-name", true, "The environment name"))
+                .addOption(new Option("r", "root-name", true, "The root workspace name"))
+                .addOption(new Option("p", "port", true, "The port on which the service is exposed")),
+            args
+        );
         final var env = CartagoEnvironment.getInstance();
-        env.init(
-            getString(config, "rootWorkspace", CartagoEnvironment.ROOT_WSP_DEFAULT_NAME),
-            getString(config, "environment", ENVIRONMENT_DEFAULT_NAME),
-            new BasicLogger()
-        );
+        env.init(parser.getOptionValue("r"), parser.getOptionValue("e"), new BasicLogger());
         env.installInfrastructureLayer("web");
-        env.startInfrastructureService(
-            "web",
-            "0.0.0.0:" + (config.hasPath("port") ? config.getInt("port") : PORT_DEFAULT_VALUE)
-        );
-    }
-
-    private static String getString(final Config config, final String key, final String orElse) {
-        return config.hasPath(key) ? config.getString(key) : orElse;
+        env.startInfrastructureService("web", "0.0.0.0:" + parser.getOptionValue("p"));
     }
 }
